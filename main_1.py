0b361f949b407217a5cfcf682b2b7ca16a547ddb
atzelote <m.noeldner@campus.tu-berlin.de>
2023-03-07 16:05:35 +0100

    07.03 Ende des Tages

--- main_1.py
+++ main_1.py
@@ -2,17 +2,18 @@ import random
 import math
 import pygame
 import matplotlib.pyplot as plt
+import numpy as np
 
 pygame.init()
 
-#Setup Fenster
+# Setup Fenster
 WIDTH, HEIGHT = 700, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Brownsche Bewegung")
 
 DO_VELOCITY_PLOT = False
 
-#Farbwerte
+# Farbwerte
 RED = (255, 82, 32)
 BLACK = (0, 0, 0)
 BLUE = (23, 2, 255)
@@ -20,13 +21,13 @@ WHITE = (255, 255, 255)
 GREEN = (50, 205, 50)
 YELLOW = (255, 255, 0)
 
-#Tick-Faktor
+# Tick-Faktor
 SPEDING = 2
-#REPULSE = 0.1
+# REPULSE = 0.1
 
-#Setup Teilchen
-BALL_RADIUS = 5
-BALL_AMOUNT = 100
+# Setup Teilchen
+BALL_RADIUS = 10
+BALL_AMOUNT = 20
 BALL_COLOR = BLUE
 BALL_MASSE = 5
 
@@ -34,24 +35,27 @@ BROWNSCHESTEILCHEN_MASSE = 10
 BROWNSCHESTEILCHEN_RADIUS = 10
 BROWNSCHESTEILCHEN_COLOR = RED
 
+
 class Ball:
     '''Klasse für alle stoßenden Teilchen'''
     SPEEDING = SPEDING
 
     def __init__(self, radius, color, masse):
-        #Startwert und aktuelle Position
+        # Startwert und aktuelle Position
         self.x = self.original_x = random.randint(0 + radius, WIDTH - radius)
         self.y = self.original_y = random.randint(0 + radius, HEIGHT - radius)
         self.radius = radius
-        #zufälliger, normierter Start-Geschwindigkeitsvektor
+        # zufälliger, normierter Start-Geschwindigkeitsvektor
         self.x_vel = random.uniform(-1, 1)
         self.y_vel = random.uniform(-1, 1)
         norm = math.sqrt(self.x_vel ** 2 + self.y_vel ** 2)
         self.x_vel = self.x_vel / norm
         self.y_vel = self.y_vel / norm
         self.masse = masse
-
+        self.position = np.array([0, 0])
+        self.acceleration = np.array([0, 0])
         self.color = color
+        self.last_collision = None  # Teilchen mit dem self als letztes kollidert sind
 
     def draw(self, win):
         '''zeichnet das Teilchen ins Fenster'''
@@ -61,46 +65,62 @@ class Ball:
         '''bewegt das Teilchen um die Geschwindigkeit'''
         self.x += self.x_vel * self.SPEEDING
         self.y += self.y_vel * self.SPEEDING
-    
-    def move_debug(self,x,y):
+
+    def move_debug(self, x, y):
         '''bewegt das Teilche um einen festen Wert'''
         self.x += x
         self.y += y
-        
+
     def handle_border_collision(self):
         '''überprüft Kollision mit der Wand, invertiert Geschwindigkeitskomponente
         und setzt das Teilchen zurück ins Fenster'''
         if self.y + self.radius >= HEIGHT:
             self.y_vel *= -1
-            self.move_debug(0, -((self.y +self.radius)-HEIGHT))
+            self.move_debug(0, -((self.y + self.radius) - HEIGHT))
+            self.last_collision = None
         elif self.y - self.radius <= 0:
             self.y_vel *= -1
-            self.move_debug(0, (-self.y +self.radius))
+            self.move_debug(0, (-self.y + self.radius))
+            self.last_collision = None
         if self.x + self.radius >= WIDTH:
             self.x_vel *= -1
-            self.move_debug(-((self.x+self.radius)-WIDTH),0)
+            self.move_debug(-((self.x + self.radius) - WIDTH), 0)
+            self.last_collision = None
         elif self.x - self.radius <= 0:
             self.x_vel *= -1
-            self.move_debug(-self.x+self.radius,0)
+            self.move_debug(-self.x + self.radius, 0)
+            self.last_collision = None
+
+    def distance(self, b2):
+        ab_x = b2.x - self.x
+        ab_y = b2.y - self.y
+        return math.sqrt(ab_x ** 2 + ab_y ** 2)
 
     def handle_collision(self, b2):
         '''überprüft Kollision mit anderen Teilchen und berechnet neue Geschwindigkeit'''
-        ab_x = b2.x - self.x
-        ab_y = b2.y - self.y
-        distance = math.sqrt(ab_x** 2 + ab_y** 2)       
-        if distance <= self.radius + b2.radius:
-            #Zwischenspeicher für Geschwindigkeiten
-            b1_x_vel = self.x_vel 
-            b1_y_vel = self.y_vel 
-            b2_x_vel = b2.x_vel 
-            b2_y_vel = b2.y_vel 
-            
-            #Berechnung elastischer Stoß
+        count = 0
+        abstand = self.distance(b2)
+        if abstand <= self.radius + b2.radius and not (self.last_collision == b2 and b2.last_collision == self):
+            # Zwischenspeicher für Geschwindigkeiten
+            b1_x_vel = self.x_vel
+            b1_y_vel = self.y_vel
+            b2_x_vel = b2.x_vel
+            b2_y_vel = b2.y_vel
+
+            # Berechnung elastischer Stoß
             self.x_vel = (self.masse * b1_x_vel + b2.masse * (2 * b2_x_vel - b1_x_vel)) / (self.masse + b2.masse)
             self.y_vel = (self.masse * b1_y_vel + b2.masse * (2 * b2_y_vel - b1_y_vel)) / (self.masse + b2.masse)
             b2.x_vel = (b2.masse * b2_x_vel + self.masse * (2 * b1_x_vel - b2_x_vel)) / (self.masse + b2.masse)
             b2.y_vel = (b2.masse * b2_y_vel + self.masse * (2 * b1_y_vel - b2_y_vel)) / (self.masse + b2.masse)
 
+            self.last_collision = b2
+            b2.last_collision = self
+            count += 1
+        abstand2 = self.distance(b2)
+        if abstand2 > self.radius + b2.radius and count > 0:
+            self.last_collision = None
+
+    # idee: problem ist dass die teilchen nicht weit genug zurückgehen und dann wieder in die andere richtung fliegen -> count einführen?
     '''def repulse(self,b2):
         ab_x = b2.x - self.x
         ab_y = b2.y - self.y
@@ -109,10 +129,11 @@ class Ball:
             repulse_force = -REPULSE/((distance**5)*self.masse)
             self.x_vel += (ab_x/distance)*repulse_force
             self.y_vel += (ab_y/distance)*repulse_force'''
-        
+
     def norm(self):
         '''berechnet den Betrag'''
         return math.sqrt(self.x_vel ** 2 + self.y_vel ** 2)
+
 
 def draw(win, balls):
     '''animiert Teilchen'''
@@ -139,7 +160,7 @@ def generate_balls(amount):
 
 def main():
     run = 1
-    #generiert das brownsche Teilchen als ersten Eintrag einer Liste aller Teilchen
+    # generiert das brownsche Teilchen als ersten Eintrag einer Liste aller Teilchen
     brownsches_teilchen = Ball(BROWNSCHESTEILCHEN_RADIUS, BROWNSCHESTEILCHEN_COLOR, BROWNSCHESTEILCHEN_MASSE)
     balls = [brownsches_teilchen] + generate_balls(BALL_AMOUNT)
     vel_dict = {}
@@ -147,8 +168,8 @@ def main():
     while run:
         draw(WIN, balls)
         pygame.time.Clock().tick(60)
-        
-        #prüft, ob das Fenster geschlosen wurde und beendet das Programm
+
+        # prüft, ob das Fenster geschlosen wurde und beendet das Programm
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = 0
@@ -158,7 +179,7 @@ def main():
             ball.handle_border_collision()
             for i in range(balls.index(ball) + 1, len(balls)):
                 ball.handle_collision(balls[i])
-                #ball.repulse(balls[i])
+                # ball.repulse(balls[i])
             ball.move()
             if DO_VELOCITY_PLOT == True:
                 '''falls True, wird die Geschwindigkeitsverteilung über 
@@ -168,13 +189,12 @@ def main():
                     vel_dict[abselv] += 1
                 else:
                     vel_dict[abselv] = 1
-                    
+
     if DO_VELOCITY_PLOT == True:
         '''falls True, wird nach Simulationsende die Geschwindigkeitsverteilung geplottet'''
         for key in vel_dict:
             plt.bar(key, vel_dict[key], 0.1, color="blue")
             plt.show()
-
 
     pygame.quit()
 
