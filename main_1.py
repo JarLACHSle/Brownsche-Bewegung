cef7ffb4be227eb95f765f43ffd3860233e86870
atzelote <m.noeldner@campus.tu-berlin.de>
2023-03-07 20:53:37 +0100

    Die Position und der Geschwindigkeitsvektor sind nun numpy Arrays (noch 2D)

--- main_1.py
+++ main_1.py
@@ -6,6 +6,8 @@ import numpy as np
 
 pygame.init()
 
+# v python 3d grafik
+
 # Setup Fenster
 WIDTH, HEIGHT = 700, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,7 +24,7 @@ GREEN = (50, 205, 50)
 YELLOW = (255, 255, 0)
 
 # Tick-Faktor
-SPEDING = 2
+SPEEDING = 2
 # REPULSE = 0.1
 
 # Setup Teilchen
@@ -38,89 +40,84 @@ BROWNSCHESTEILCHEN_COLOR = RED
 
 class Ball:
     '''Klasse für alle stoßenden Teilchen'''
-    SPEEDING = SPEDING
 
     def __init__(self, radius, color, masse):
         # Startwert und aktuelle Position
         self.x = self.original_x = random.randint(0 + radius, WIDTH - radius)
         self.y = self.original_y = random.randint(0 + radius, HEIGHT - radius)
-        self.radius = radius
+        self.position = np.array([self.x, self.y], dtype=float)
+        self.zeitschritt = SPEEDING
+
         # zufälliger, normierter Start-Geschwindigkeitsvektor
         self.x_vel = random.uniform(-1, 1)
         self.y_vel = random.uniform(-1, 1)
         norm = math.sqrt(self.x_vel ** 2 + self.y_vel ** 2)
         self.x_vel = self.x_vel / norm
         self.y_vel = self.y_vel / norm
+        self.vel_vec = np.array([self.x_vel, self.y_vel])
+
+        self.radius = radius
+
         self.masse = masse
-        self.position = np.array([0, 0])
         self.acceleration = np.array([0, 0])
         self.color = color
         self.last_collision = None  # Teilchen mit dem self als letztes kollidert sind
 
     def draw(self, win):
         '''zeichnet das Teilchen ins Fenster'''
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+        pygame.draw.circle(win, self.color, (self.position[0], self.position[1]), self.radius)
 
     def move(self):
         '''bewegt das Teilchen um die Geschwindigkeit'''
-        self.x += self.x_vel * self.SPEEDING
-        self.y += self.y_vel * self.SPEEDING
+        self.position += self.vel_vec * self.zeitschritt
 
-    def move_debug(self, x, y):
+    def move_debug(self, moving):
         '''bewegt das Teilche um einen festen Wert'''
-        self.x += x
-        self.y += y
+        self.position += moving
 
     def handle_border_collision(self):
         '''überprüft Kollision mit der Wand, invertiert Geschwindigkeitskomponente
         und setzt das Teilchen zurück ins Fenster'''
-        if self.y + self.radius >= HEIGHT:
-            self.y_vel *= -1
-            self.move_debug(0, -((self.y + self.radius) - HEIGHT))
+        if self.position[1] + self.radius >= HEIGHT:
+            self.vel_vec[1] *= -1
+            self.move_debug(np.array([0, -((self.position[1] + self.radius) - HEIGHT)]))
             self.last_collision = None
-        elif self.y - self.radius <= 0:
-            self.y_vel *= -1
-            self.move_debug(0, (-self.y + self.radius))
+        elif self.position[1] - self.radius <= 0:
+            self.vel_vec[1] *= -1
+            self.move_debug(np.array([0, - self.position[1] + self.radius]))
             self.last_collision = None
-        if self.x + self.radius >= WIDTH:
-            self.x_vel *= -1
-            self.move_debug(-((self.x + self.radius) - WIDTH), 0)
+        if self.position[0] + self.radius >= WIDTH:
+            self.vel_vec[0] *= -1
+            self.move_debug(np.array([-((self.position[0] + self.radius) - WIDTH), 0]))
             self.last_collision = None
-        elif self.x - self.radius <= 0:
-            self.x_vel *= -1
-            self.move_debug(-self.x + self.radius, 0)
+        elif self.position[0] - self.radius <= 0:
+            self.vel_vec[0] *= -1
+            self.move_debug(np.array([-self.position[0] + self.radius, 0]))
             self.last_collision = None
-
-    def distance(self, b2):
-        ab_x = b2.x - self.x
-        ab_y = b2.y - self.y
-        return math.sqrt(ab_x ** 2 + ab_y ** 2)
 
     def handle_collision(self, b2):
-        '''überprüft Kollision mit anderen Teilchen und berechnet neue Geschwindigkeit'''
+        """überprüft Kollision mit anderen Teilchen und berechnet neue Geschwindigkeit"""
         count = 0
-        abstand = self.distance(b2)
+        abstand = np.linalg.norm(b2.position - self.position)
         if abstand <= self.radius + b2.radius and not (self.last_collision == b2 and b2.last_collision == self):
             # Zwischenspeicher für Geschwindigkeiten
-            b1_x_vel = self.x_vel
-            b1_y_vel = self.y_vel
-            b2_x_vel = b2.x_vel
-            b2_y_vel = b2.y_vel
+            b1_vel_vec = self.vel_vec
+            b2_vel_vec = b2.vel_vec
 
             # Berechnung elastischer Stoß
-            self.x_vel = (self.masse * b1_x_vel + b2.masse * (2 * b2_x_vel - b1_x_vel)) / (self.masse + b2.masse)
-            self.y_vel = (self.masse * b1_y_vel + b2.masse * (2 * b2_y_vel - b1_y_vel)) / (self.masse + b2.masse)
-            b2.x_vel = (b2.masse * b2_x_vel + self.masse * (2 * b1_x_vel - b2_x_vel)) / (self.masse + b2.masse)
-            b2.y_vel = (b2.masse * b2_y_vel + self.masse * (2 * b1_y_vel - b2_y_vel)) / (self.masse + b2.masse)
+            self.vel_vec = (self.masse * b1_vel_vec + b2.masse * (2 * b2_vel_vec - b1_vel_vec)) / (
+                        self.masse + b2.masse)
+            b2.vel_vec = (b2.masse * b2_vel_vec + self.masse * (2 * b1_vel_vec - b2.vel_vec)) / (self.masse + b2.masse)
 
             self.last_collision = b2
             b2.last_collision = self
             count += 1
-        abstand2 = self.distance(b2)
+        abstand2 = np.linalg.norm(b2.position - self.position)
         if abstand2 > self.radius + b2.radius and count > 0:
             self.last_collision = None
 
-    # idee: problem ist dass die teilchen nicht weit genug zurückgehen und dann wieder in die andere richtung fliegen -> count einführen?
+    # idee: problem ist dass die teilchen nicht weit genug zurückgehen und dann wieder in die andere richtung fliegen
+    # -> count einführen?
     '''def repulse(self,b2):
         ab_x = b2.x - self.x
         ab_y = b2.y - self.y
@@ -130,10 +127,6 @@ class Ball:
             self.x_vel += (ab_x/distance)*repulse_force
             self.y_vel += (ab_y/distance)*repulse_force'''
 
-    def norm(self):
-        '''berechnet den Betrag'''
-        return math.sqrt(self.x_vel ** 2 + self.y_vel ** 2)
-
 
 def draw(win, balls):
     '''animiert Teilchen'''
@@ -141,11 +134,6 @@ def draw(win, balls):
     for ball in balls:
         ball.draw(win)
     pygame.display.update()
-
-
-def skalarprodukt(v1, v2):
-    '''2D-Skalarprodukt'''
-    return v1[0] * v2[0] + v1[1] * v2[1]
 
 
 def generate_balls(amount):
@@ -181,7 +169,7 @@ def main():
                 ball.handle_collision(balls[i])
                 # ball.repulse(balls[i])
             ball.move()
-            if DO_VELOCITY_PLOT == True:
+            if DO_VELOCITY_PLOT:
                 '''falls True, wird die Geschwindigkeitsverteilung über 
                 den gesamten Verlauf der Simulation aufgezeichnet'''
                 abselv = math.sqrt(ball.x_vel ** 2 + ball.y_vel ** 2)
@@ -190,7 +178,7 @@ def main():
                 else:
                     vel_dict[abselv] = 1
 
-    if DO_VELOCITY_PLOT == True:
+    if DO_VELOCITY_PLOT:
         '''falls True, wird nach Simulationsende die Geschwindigkeitsverteilung geplottet'''
         for key in vel_dict:
             plt.bar(key, vel_dict[key], 0.1, color="blue")
